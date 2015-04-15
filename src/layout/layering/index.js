e53fb4d5a68046d136c25985b70a70f7fec8989e
Yosuke ONOUE <onoue@likr-lab.com>
2015-04-15 20:57:48 +0900

    Add layering

---
+++ src/layout/layering/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = function layering(g) {
+  var layers = {},
+      queue = [];
+  for (let u of g.vertices()) {
+    if (g.inDegree(u) === 0) {
+      layers[u] = 0;
+      queue.push(u);
+    }
+  }
+  while (queue.length > 0) {
+    let u = queue.shift();
+    for (let v of g.outVertices(u)) {
+      if (layers[v] === undefined || layers[v] < layers[u] + 1) {
+        layers[v] = layers[u] + 1;
+      }
+      queue.push(v);
+    }
+  }
+  return layers;
+};
---
+++ test/layout/layering.js
@@ -0,0 +1,25 @@
+'use strict';
+var expect = require('expect.js'),
+    graph = require('../../lib/graph'),
+    layering = require('../../lib/layout/layering');
+
+describe('layering(g)', () => {
+  it('returns layers', () => {
+    var g = graph();
+    var a = g.addVertex();
+    var b = g.addVertex();
+    var c = g.addVertex();
+    var d = g.addVertex();
+    var e = g.addVertex();
+    g.addEdge(a, b);
+    g.addEdge(b, c);
+    g.addEdge(d, c);
+    g.addEdge(d, e);
+    var layers = layering(g);
+    expect(layers[a]).to.be(0);
+    expect(layers[b]).to.be(1);
+    expect(layers[c]).to.be(2);
+    expect(layers[d]).to.be(0);
+    expect(layers[e]).to.be(1);
+  });
+});
