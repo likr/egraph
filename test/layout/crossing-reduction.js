71a82d4c83c99d23546fde46f6cb5f972396d7b9
Yosuke ONOUE <onoue@likr-lab.com>
2015-04-16 14:21:44 +0900

    Add baryCenter

--- test/layout/crossing-reduction.js
+++ test/layout/crossing-reduction.js
@@ -1,24 +1,48 @@
 'use strict';
 var expect = require('expect.js'),
-    getGraph = require('../../lib/graph'),
-    cross = require('../../lib/layout/crossing-reduction/cross');
+    graph = require('../../lib/graph'),
+    cross = require('../../lib/layout/crossing-reduction/cross'),
+    baryCenter = require('../../lib/layout/crossing-reduction/bary-center');
 
 describe('cross(g, h1, h2)', () => {
   it('returns the number of crossing', () => {
-    var graph = getGraph();
-    var a = graph.addVertex();
-    var b = graph.addVertex();
-    var c = graph.addVertex();
-    var d = graph.addVertex();
-    var e = graph.addVertex();
-    var f = graph.addVertex();
-    var g = graph.addVertex();
-    graph.addEdge(a, f);
-    graph.addEdge(b, f);
-    graph.addEdge(b, g);
-    graph.addEdge(c, e);
-    graph.addEdge(c, g);
-    graph.addEdge(d, f);
-    expect(cross(graph, [a, b, c, d], [e, f, g])).to.be(5);
+    let g = graph();
+    let u1 = g.addVertex();
+    let u2 = g.addVertex();
+    let u3 = g.addVertex();
+    let u4 = g.addVertex();
+    let v1 = g.addVertex();
+    let v2 = g.addVertex();
+    let v3 = g.addVertex();
+    g.addEdge(u1, v2);
+    g.addEdge(u2, v2);
+    g.addEdge(u2, v3);
+    g.addEdge(u3, v1);
+    g.addEdge(u3, v3);
+    g.addEdge(u4, v2);
+    expect(cross(g, [u1, u2, u3, u4], [v1, v2, v3])).to.be(5);
+  });
+});
+
+describe('baryCenter(g, h1, h2)', () => {
+  it('sorts h2', () => {
+    let g = graph();
+    let u1 = g.addVertex();
+    let u2 = g.addVertex();
+    let u3 = g.addVertex();
+    let u4 = g.addVertex();
+    let v1 = g.addVertex();
+    let v2 = g.addVertex();
+    let v3 = g.addVertex();
+    g.addEdge(u1, v2);
+    g.addEdge(u2, v2);
+    g.addEdge(u2, v3);
+    g.addEdge(u3, v1);
+    g.addEdge(u3, v3);
+    g.addEdge(u4, v2);
+    let h1 = [u1, u2, u3, u4],
+        h2 = [v1, v2, v3];
+    baryCenter(g, h1, h2);
+    expect(h2).to.be.eql([v2, v3, v1]);
   });
 });
