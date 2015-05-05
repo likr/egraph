649db8ca7a3ce9e9c7c0dbd2f12e6fdbafedda28
Yosuke ONOUE <onoue@likr-lab.com>
2015-05-05 12:50:20 +0900

    Add test

---
+++ test/renderer/index.js
@@ -0,0 +1,89 @@
+'use strict';
+
+import expect from 'expect.js';
+import Renderer from '../../src/renderer';
+import Layouter from '../../src/layout';
+import VertexRenderer from '../../src/renderer/vertex-renderer/circle-vertex-renderer';
+import EdgeRenderer from '../../src/renderer/edge-renderer/straight-edge-renderer';
+
+describe('Renderer', () => {
+  describe('vertexVisibility()', () => {
+    it('returns current vertexVisibility function', () => {
+      const renderer = new Renderer();
+      expect(renderer.vertexVisibility()).to.be.a('function');
+    });
+  });
+
+  describe('vertexVisibility(f)', () => {
+    it('sets vertexVisibility and returns self', () => {
+      const renderer = new Renderer(),
+            f = () => true;
+      expect(renderer.vertexVisibility(f)).to.be(renderer);
+      expect(renderer.vertexVisibility()).to.be(f);
+    });
+  });
+
+  describe('edgeVisibility()', () => {
+    it('returns current edgeVisibility function', () => {
+      const renderer = new Renderer();
+      expect(renderer.edgeVisibility()).to.be.a('function');
+    });
+  });
+
+  describe('edgeVisibility(f)', () => {
+    it('sets edgeVisibility and returns self', () => {
+      const renderer = new Renderer(),
+            f = () => true;
+      expect(renderer.edgeVisibility(f)).to.be(renderer);
+      expect(renderer.edgeVisibility()).to.be(f);
+    });
+  });
+
+  describe('layouter()', () => {
+    it('returns current layouter instance', () => {
+      const renderer = new Renderer();
+      expect(renderer.layouter()).to.have.property('layout');
+    });
+  });
+
+  describe('layouter(layouter)', () => {
+    it('sets layouter and returns self', () => {
+      const renderer = new Renderer(),
+            layouter = new Layouter();
+      expect(renderer.layouter(layouter)).to.be(renderer);
+      expect(renderer.layouter()).to.be(layouter);
+    });
+  });
+
+  describe('vertexRenderer()', () => {
+    it('returns current vertexRenderer instance', () => {
+      const renderer = new Renderer();
+      expect(renderer.vertexRenderer()).to.have.property('render');
+    });
+  });
+
+  describe('vertexRenderer(vertexRenderer)', () => {
+    it('sets vertexRenderer and returns self', () => {
+      const renderer = new Renderer(),
+            vertexRenderer = new VertexRenderer();
+      expect(renderer.vertexRenderer(vertexRenderer)).to.be(renderer);
+      expect(renderer.vertexRenderer()).to.be(vertexRenderer);
+    });
+  });
+
+  describe('edgeRenderer()', () => {
+    it('returns current edgeRenderer function', () => {
+      const renderer = new Renderer();
+      expect(renderer.edgeRenderer()).to.have.property('render');
+    });
+  });
+
+  describe('edgeRenderer(edgeRenderer)', () => {
+    it('sets edgeRenderer and returns self', () => {
+      const renderer = new Renderer(),
+            edgeRenderer = new EdgeRenderer();
+      expect(renderer.edgeRenderer(edgeRenderer)).to.be(renderer);
+      expect(renderer.edgeRenderer()).to.be(edgeRenderer);
+    });
+  });
+});
