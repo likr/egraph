6f26cc436af626cb5ac8d36f8c5c8cbff311bff2
Yosuke ONOUE <onoue@likr-lab.com>
2015-09-15 16:03:27 +0900

    Remove example

--- gulpfile.js
+++ gulpfile.js
@@ -17,20 +17,8 @@ gulp.task('babel', function() {
 gulp.task('browserify', function() {
   return browserify({entries: ['./lib/index.js']})
     .bundle()
-    .pipe(source('eg-graph.js'))
+    .pipe(source('egraph.js'))
     .pipe(gulp.dest('.'));
-});
-
-gulp.task('example', function() {
-  return browserify({entries: ['./example/src/index.js']})
-    .transform(babelify.configure({optional: ['runtime']}))
-    .bundle()
-    .on('error', function (err) {
-      console.log(err.message);
-      this.emit('end');
-    })
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('example'));
 });
 
 gulp.task('mocha', shell.task(['mocha --recursive --colors --reporter dot --require misc/babel_hook.js']));
@@ -42,9 +30,8 @@ gulp.task('test', ['mocha']);
 gulp.task('release', ['babel'], shell.task(['npm publish']));
 
 gulp.task('watch', function() {
-  gulp.watch('src/**/*.js', ['mocha', 'example']);
+  gulp.watch('src/**/*.js', ['mocha']);
   gulp.watch(['test/**/*.js'], ['mocha']);
-  gulp.watch(['example/src/**/*.js', 'example/**/*.html'], ['example']);
 });
 
 gulp.task('default', ['build']);
