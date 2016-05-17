f506b31a0f0c8426da4c6c19ac6eb0d938df7b0c
Yosuke ONOUE <onoue@likr-lab.com>
2016-05-18 08:57:49 +0900

    Fix completeQB

--- test/transformer/edge-concentration/complete-qb.js
+++ test/transformer/edge-concentration/complete-qb.js
@@ -3,6 +3,31 @@ const assert = require('power-assert')
 const Graph = require('../../../graph')
 const completeQB = require('../../../transformer/edge-concentration/complete-qb')
 
+const testEpsilonQuasiBiclique = (graph, source, target, epsilon, ms) => {
+  assert(source.length >= ms)
+  assert(target.length >= ms)
+  for (const u of source) {
+    const vertices = new Set(graph.outVertices(u))
+    let count = 0
+    for (const v of target) {
+      if (!vertices.has(v)) {
+        count += 1
+      }
+    }
+    assert(count <= epsilon)
+  }
+  for (const u of target) {
+    const vertices = new Set(graph.inVertices(u))
+    let count = 0
+    for (const v of source) {
+      if (!vertices.has(v)) {
+        count += 1
+      }
+    }
+    assert(count <= epsilon)
+  }
+}
+
 describe('completeQb(graph, h1, h2, epsilon)', () => {
   it('return maximal epsilon-quasi-bicliques', () => {
     const h1 = [1, 2, 3, 4, 5, 6]
@@ -34,12 +59,42 @@ describe('completeQb(graph, h1, h2, epsilon)', () => {
     graph.addEdge(5, 12)
     graph.addEdge(6, 12)
 
-    assert.deepEqual(completeQB(graph, h1, h2, 1, 3), [
-      {source: [1, 2, 3, 4], target: [10, 11, 12, 9]},
-      {source: [2, 3, 4, 5], target: [10, 11, 12, 9]},
-      {source: [2, 3, 4, 6], target: [10, 11, 12, 9]},
-      {source: [1, 2, 3, 4, 5], target: [10, 11, 12]},
-      {source: [1, 2, 3, 4, 6], target: [10, 11, 12]}
+    const result = completeQB(graph, h1, h2, 1, 3)
+    for (const {source, target} of result) {
+      testEpsilonQuasiBiclique(graph, source, target, 1, 3)
+    }
+    assert.deepEqual(result, [
+      {source: [1, 2, 3, 4], target: [10, 11, 12, 9]}
+    ])
+  })
+
+  it('return maximal epsilon-quasi-bicliques', () => {
+    const h1 = [0, 1, 2, 3]
+    const h2 = [4, 5, 6, 7]
+    const graph = new Graph()
+    for (const u of h1) {
+      graph.addVertex(u)
+    }
+    for (const u of h2) {
+      graph.addVertex(u)
+    }
+    graph.addEdge(0, 4)
+    graph.addEdge(1, 5)
+    graph.addEdge(1, 6)
+    graph.addEdge(1, 7)
+    graph.addEdge(2, 5)
+    graph.addEdge(2, 6)
+    graph.addEdge(2, 7)
+    graph.addEdge(3, 5)
+    graph.addEdge(3, 6)
+    graph.addEdge(3, 7)
+
+    const result = completeQB(graph, h1, h2, 1, 3)
+    for (const {source, target} of result) {
+      testEpsilonQuasiBiclique(graph, source, target, 1, 3)
+    }
+    assert.deepEqual(result, [
+      {source: [1, 2, 3], target: [5, 6, 7]}
     ])
   })
 })
--- transformer/edge-concentration/complete-qb.js
+++ transformer/edge-concentration/complete-qb.js
@@ -2,14 +2,17 @@ const {combination} = require('js-combinatorics')
 
 const enumerate = function * (neighbors, epsilon) {
   if (neighbors.size > 0) {
-    const iter = combination(Array.from(neighbors), Math.min(epsilon, neighbors.size))
-    while (true) {
-      const S = iter.next()
-      if (!S) {
-        break
+    for (let i = epsilon; i > 0; --i) {
+      const iter = combination(Array.from(neighbors), Math.min(i, neighbors.size))
+      while (true) {
+        const S = iter.next()
+        if (!S) {
+          break
+        }
+        yield S
       }
-      yield S
     }
+    yield []
   }
 }
 
@@ -31,52 +34,113 @@ const genKey = (Vl, Vr) => {
   return `${arrayVl.join(',')}:${arrayVr.join(',')}`
 }
 
-const subspace = (graph, candVl, candExt, epsilon, ms, result) => {
-  const candVr = adjacentVertices(graph, candVl)
-  for (const v of candVr) {
+const countError = (graph, u, vertices, ltou) => {
+  const neighbors = new Set(ltou ? graph.inVertices(u) : graph.outVertices(u))
+  let count = 0
+  for (const v of vertices) {
+    if (!neighbors.has(v)) {
+      count += 1
+    }
+  }
+  return count
+}
+
+const intersection = (A, B) => {
+  const result = new Set()
+  for (const item of A) {
+    if (B.has(item)) {
+      result.add(item)
+    }
+  }
+  return result
+}
+
+const setminus = (A, B) => {
+  for (const item of B) {
+    A.delete(item)
+  }
+  return A
+}
+
+const store = (result, key, Vl, Vr) => {
+  for (const [key, {source, target}] of result.entries()) {
+    const sourceIntersection = intersection(source, Vl)
+    const targetIntersection = intersection(target, Vr)
+    if (sourceIntersection.size === source.size && targetIntersection.size === target.size) {
+      result.delete(key)
+    } else if (sourceIntersection.size === Vl.size && targetIntersection.size === Vr.size) {
+      return
+    }
+  }
+  result.set(key, {source: Vl, target: Vr})
+}
+
+const testEpsilonQuasiBiclique = (graph, source, target, epsilon, ms) => {
+  if (source.size < ms || target.size < ms) {
+    return false
+  }
+  for (const u of source) {
+    const vertices = new Set(graph.outVertices(u))
     let count = 0
-    for (const u of graph.inVertices(v)) {
-      if (candVl.has(u)) {
+    for (const v of target) {
+      if (!vertices.has(v)) {
         count += 1
       }
     }
-    if (candVl.size - count > epsilon) {
+    if (count > epsilon) {
+      return false
+    }
+  }
+  for (const u of target) {
+    const vertices = new Set(graph.inVertices(u))
+    let count = 0
+    for (const v of source) {
+      if (!vertices.has(v)) {
+        count += 1
+      }
+    }
+    if (count > epsilon) {
+      return false
+    }
+  }
+  return true
+}
+
+const subspace = (graph, candVl, genVl, candExt, epsilon, ms, visited, result) => {
+  const candVr = adjacentVertices(graph, candVl)
+  for (const v of candVr) {
+    if (countError(graph, v, candVl, true) > epsilon) {
       candVr.delete(v)
     }
   }
+
   const key = genKey(candVl, candVr)
-  if (result.has(key)) {
+  if (visited.has(key)) {
     return
   }
-  if (candVl.size >= ms && candVr.size >= ms) {
-    const source = Array.from(candVl)
-    const target = Array.from(candVr)
-    source.sort()
-    target.sort()
-    result.set(key, {source, target})
+  visited.add(key)
+  if (testEpsilonQuasiBiclique(graph, candVl, candVr, epsilon, ms)) {
+    store(result, key, candVl, candVr)
   }
-  for (const v of candVr) {
-    candExt.delete(v)
-  }
+
+  setminus(candExt, candVr)
   for (const v of candExt) {
-    const candExt2 = new Set(candExt)
-    candExt2.delete(v)
-    const neighbors = new Set(candVl)
-    for (const u of graph.inVertices(v)) {
-      neighbors.delete(u)
-    }
-    for (const S of enumerate(neighbors, epsilon)) {
-      const Vl = new Set(graph.inVertices(v))
+    candExt.delete(v)
+    const neighbors = intersection(candVl, new Set(graph.inVertices(v)))
+    const rest = setminus(new Set(candVl), neighbors)
+    for (const S of enumerate(rest, epsilon)) {
+      const Vl = new Set(neighbors)
       for (const u of S) {
         Vl.add(u)
       }
-      subspace(graph, Vl, candExt2, epsilon, ms, result)
+      subspace(graph, Vl, v, candExt, epsilon, ms, visited, result)
     }
   }
 }
 
 const completeQB = (graph, h1, h2, epsilon, ms) => {
   const bicliques = new Map()
+  const visited = new Set()
   for (const v of h2) {
     const neighbors = new Set(h1)
     for (const u of graph.inVertices(v)) {
@@ -87,10 +151,19 @@ const completeQB = (graph, h1, h2, epsilon, ms) => {
       for (const u of S) {
         Vl.add(u)
       }
-      subspace(graph, Vl, new Set(h2), epsilon, ms, bicliques)
+      subspace(graph, Vl, v, new Set(h2), epsilon, ms, visited, bicliques)
     }
   }
-  return Array.from(bicliques.values())
+  return Array.from(bicliques.values()).map(({source, target}) => {
+    const sourceArray = Array.from(source)
+    const targetArray = Array.from(target)
+    sourceArray.sort()
+    targetArray.sort()
+    return {
+      source: sourceArray,
+      target: targetArray
+    }
+  })
 }
 
 module.exports = completeQB
