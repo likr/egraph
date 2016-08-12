7ece99b0f94f6a9ec1c0188cd55897aa35c64f60
Yosuke ONOUE <onoue@likr-lab.com>
2016-08-12 22:45:51 +0900

    Add layer assignment for compound graph

--- layouter/sugiyama-compound/layer-assignment.js
+++ layouter/sugiyama-compound/layer-assignment.js
@@ -1,13 +1,38 @@
-const layerAssignmentRec = (graph, root, clev) => {
-}
+const Graph = require('../../graph')
+const LongestPath = require('../sugiyama/layer-assignment/longest-path')
 
 const layerAssignment = (graph) => {
   const result = new Map()
-  for (const u of graph.vertices()) {
-    if (graph.parent(u)) {
-      continue
+  const longestPath = new LongestPath()
+  let children = new Set(graph.vertices().filter((u) => graph.parent(u) === null))
+  while (children.size) {
+    const subGraph = new Graph()
+    for (const u of children) {
+      subGraph.addVertex(u)
     }
-    layerAssignmentRec(graph, u, [0])
+    for (const [u, v] of graph.edges()) {
+      if (subGraph.vertex(u) && subGraph.vertex(v)) {
+        subGraph.addEdge(u, v)
+      }
+    }
+    const layers = longestPath.call(subGraph)
+    for (const u of subGraph.vertices()) {
+      if (graph.parent(u)) {
+      }
+      const parentLayer = graph.parent(u) === null
+        ? []
+        : Array.from(result.get(graph.parent(u)))
+      parentLayer.push(layers[u])
+      result.set(u, parentLayer)
+    }
+
+    const parents = children
+    children = new Set()
+    for (const u of parents) {
+      for (const v of graph.children(u)) {
+        children.add(v)
+      }
+    }
   }
   return result
 }
